2a86832f2809814fecf18bef80d3853dec351004
Aditya Soni <54634472+adityasoni2019@users.noreply.github.com>
2023-01-13 19:26:28 +0530

    Test commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// npm install -g firebase-tools
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA032cGT6kiVmj7FrshKO_blQACHQ4c7OM",
+    authDomain: "netflix-clone-6cc08.firebaseapp.com",
+    projectId: "netflix-clone-6cc08",
+    storageBucket: "netflix-clone-6cc08.appspot.com",
+    messagingSenderId: "403566390897",
+    appId: "1:403566390897:web:dd88f3b3bd1e6afef0b5d0"
+  };
+
