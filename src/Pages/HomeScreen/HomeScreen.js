3b85c696b8adb9e0cc892d427055a840c0a8aa6e
Aditya Soni <54634472+adityasoni2019@users.noreply.github.com>
2023-01-16 19:29:41 +0530

    Working. Added the rows. Still more to work on this, but major progress.

--- src/Pages/HomeScreen/HomeScreen.js
+++ src/Pages/HomeScreen/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import '../HomeScreen/HomeScreen.css'
 import Nav from '../../Components/Nav/Nav'
 import Banner from '../../Components/Banner/Banner'
+import requests from '../../files/Requests'
+import Row from '../../Components/Row/Row.js'
 
 const HomeScreen = () => {
   return (
@@ -12,6 +14,15 @@ const HomeScreen = () => {
         <Nav/> 
 
         <Banner/>
+
+        <Row title = "Netflix Original" fetchUrl = {requests.fetchNetflixOriginals} isLargeRow/>
+        <Row title = "Top Rated" fetchUrl = {requests.fetchTopRated}/> 
+        <Row title = "Trending Now" fetchUrl = {requests.fetchTrending}/>
+        <Row title = "Action Movies" fetchUrl = {requests.fetchActionMovies}/>
+        <Row title = "Comedy Movies" fetchUrl = {requests.fetchComedyMovies}/>
+        <Row title = "Horror Movies" fetchUrl = {requests.fetchHorrorMovies}/>
+        <Row title = "Romance Movies" fetchUrl = {requests.fetchRomanceMovies}/>
+        <Row title = "Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
 
 
     </div>
